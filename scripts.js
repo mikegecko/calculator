c6be0607c18342680a5907b1616a5b0a79e11290
Michael Guedko <mjgeko@gmail.com>
2022-02-17 14:20:52 -0500

    Setup math functions

---
+++ scripts.js
@@ -0,0 +1,36 @@
+/*
+
+
+
+
+*/
+function add(a,b){
+    return a+b;
+}
+function subtract(a,b){
+    return a-b;
+}
+function multiply(a,b){
+    return a*b;
+}
+function divide(a,b){
+    return a/b; //Implement test for divide by 0
+}
+function operate(arg1, operator, arg2){
+    switch(operator){
+        case '+':
+            add(arg1,arg2);
+            break;
+        case '-':
+            subtract(arg1, arg2);
+            break;
+        case '*':
+            multiply(arg1, arg2);
+            break;
+        case '/':
+            divide(arg1, arg2);
+            break;
+        default:
+            console.log(`Invalid operator ${operator}`);
+    }
+}
