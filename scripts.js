ae8cd71eb08521c90c5d042351ffd3201fcfcfd7
Michael Guedko <mjgeko@gmail.com>
2022-02-19 20:53:18 -0500

    Handler stuff & added positive/negative button

--- scripts.js
+++ scripts.js
@@ -5,39 +5,39 @@ const buttons = document.querySelectorAll('.btn');
 buttons.forEach(element => {
     element.addEventListener('click', buttonHandler);
 });
-
+/*Make a function that handles the leading 0 on the display by using a handler whenever the display is updated*/
 function buttonHandler(e){
     //console.log(e);
     switch(e.target.id){
         case '9':
-            currentDisplay.innerHTML =+ '9';
+            currentDisplay.innerHTML += '9';
             break;
         case '8':
-            currentDisplay.innerHTML =+ '8';
+            currentDisplay.innerHTML += '8';
             break;
         case '7':
-            currentDisplay.innerHTML =+ '7';
+            currentDisplay.innerHTML += '7';
             break;
         case '6':
-            currentDisplay.innerHTML =+ '6';
+            currentDisplay.innerHTML += '6';
             break;
         case '5':
-            currentDisplay.innerHTML =+ '5';
+            currentDisplay.innerHTML += '5';
             break;
         case '4':
-            currentDisplay.innerHTML =+ '4';
+            currentDisplay.innerHTML += '4';
             break;
         case '3':
-            currentDisplay.innerHTML =+ '3';
+            currentDisplay.innerHTML += '3';
             break;
         case '2':
-            currentDisplay.innerHTML =+ '2';
+            currentDisplay.innerHTML += '2';
             break;
         case '1':
-            currentDisplay.innerHTML =+ '1';
+            currentDisplay.innerHTML += '1';
             break;
         case '0':
-            currentDisplay.innerHTML =+ '0';
+            currentDisplay.innerHTML += '0';
             break;
         default:
             console.log(`Handler error for ${e}`);
